f3fce784e927bec7fe434a132bc553167a08e66f
Artiom-Evs <artiom.evs@gmail.com>
2025-02-03 13:33:53 +0100

    Add configuring additional image source URLs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const checkEnvVariables = require("./check-env-variables")
 
 checkEnvVariables()
 
+const imageSources = getImageSources()
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -36,8 +38,26 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      ...imageSources,
     ],
   },
 }
 
 module.exports = nextConfig
+
+function getImageSources() {
+  const urlsString = process.env.IMAGE_SOURCE_URLS
+
+  if (!urlsString) return []
+
+  const urls = urlsString.split(",").map((url) => url.trim())
+  const regex = /^(https?):\/\/([^\/]+)/i
+
+  return urls
+    .map((url) => url.match(regex))
+    .filter(Boolean)
+    .map((match) => ({
+      protocol: match[1],
+      hostname: match[2],
+    }))
+}
